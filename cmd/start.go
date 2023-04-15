1ff95ab9e9dbc3d9e3de14d3ef5b0a35fd0ee056
Caio Vinicius <caio@vncius.com>
2023-04-15 03:11:43 -0300

    Improvements on start serbice command

--- cmd/start.go
+++ cmd/start.go
@@ -22,12 +22,22 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"os"
-	"os/exec"
+	"time"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/spf13/cobra"
 )
+
+var showLogs bool = false
 
 var startCmd = &cobra.Command{
 	Use:   "start",
@@ -40,15 +50,92 @@ var startCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(startCmd)
+	rootCmd.PersistentFlags().BoolVarP(&showLogs, "logs", "l", false, "Define if show dockenv logs")
 }
 
 func startMainService() {
-	startCommand := exec.Command("docker", "run", "-d", "--rm", "--name=dockenv-proxy", "--net", "dockenv-network", "-p", "80:80", "-v", "/var/run/docker.sock:/tmp/docker.sock:ro", "jwilder/nginx-proxy")
+	var containerName string = "dockenv-proxy"
+	appContext := context.Background()
+	dockerClient, dockerClientError := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 
-	if startCommandError := startCommand.Run(); startCommandError != nil {
-		fmt.Fprintln(os.Stderr, "Unable to start dockenv services: ", startCommandError)
+	if dockerClientError != nil {
+		panic(dockerClientError)
+	}
+
+	containersList, containersListError := dockerClient.ContainerList(appContext, types.ContainerListOptions{})
+
+	if containersListError != nil {
+		panic(containersListError)
+	}
+
+	var dockenvServiceIsRunning bool = false
+
+	for _, container := range containersList {
+		for _, name := range container.Names {
+			if name == "/"+containerName {
+				if container.State == "running" {
+					dockenvServiceIsRunning = true
+				}
+			}
+		}
+	}
+
+	if dockenvServiceIsRunning {
+		fmt.Println("Service already running")
 		return
 	}
 
+	network := &network.NetworkingConfig{
+		EndpointsConfig: map[string]*network.EndpointSettings{
+			"dockenv-network": {
+				NetworkID: "dockenv-network",
+			},
+		},
+	}
+
+	ports := nat.PortMap{
+		"80/tcp": []nat.PortBinding{
+			{
+				HostIP:   "0.0.0.0",
+				HostPort: "80",
+			},
+		},
+	}
+
+	dockenvServiceContainer, dockenvServiceContainerError := dockerClient.ContainerCreate(appContext, &container.Config{
+		Image:        "jwilder/nginx-proxy",
+		ExposedPorts: nat.PortSet{"80/tcp": struct{}{}},
+	}, &container.HostConfig{
+		NetworkMode:  container.NetworkMode("dockenv-network"),
+		Binds:        []string{"/var/run/docker.sock:/tmp/docker.sock:ro"},
+		PortBindings: ports,
+		AutoRemove:   true,
+	}, network, &v1.Platform{}, "dockenv-proxy")
+
+	if dockenvServiceContainerError != nil {
+		panic(dockenvServiceContainerError)
+	}
+
+	if dockenvStartContainerError := dockerClient.ContainerStart(context.Background(), dockenvServiceContainer.ID, types.ContainerStartOptions{}); dockenvStartContainerError != nil {
+		panic(dockenvStartContainerError)
+	}
+
 	fmt.Println("dockenv started successfully")
+	fmt.Println("")
+
+	if showLogs {
+		out, err := dockerClient.ContainerLogs(context.Background(), dockenvServiceContainer.ID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true, Follow: true})
+		if err != nil {
+			panic(err)
+		}
+		defer out.Close()
+
+		for {
+			_, err := io.Copy(os.Stdout, out)
+			if err != nil {
+				break
+			}
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -2,13 +2,23 @@ module github.com/zer062/dockenv
 
 go 1.20
 
+require github.com/docker/docker v23.0.3+incompatible
+
 require (
+	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/cobra v1.7.0 // indirect
@@ -16,8 +26,11 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.15.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
